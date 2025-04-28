cde372b37919d2b6f2b3605af52983bd5cd61557
nullptr <lia@bertran.xyz>
2025-04-28 11:37:49 +0200

    Added modified list of widgets widget

---
+++ src/list.rs
@@ -0,0 +1,10 @@
+pub use self::{
+    // item::ListItem,
+    list::{ListDirection, WidgetList, WidgetListItem},
+    state::WidgetListState,
+};
+
+// mod item;
+mod list;
+mod rendering;
+mod state;
